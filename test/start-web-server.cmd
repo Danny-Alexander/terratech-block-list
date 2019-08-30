db8a3d5d6a40fe656f4a76eb21c9eb17ce2b898a
danny-alexander <danny.alexa@gmail.com>
2019-08-30 12:06:28 +0930

    Move start-web-server.cmd into test folder

--- test/start-web-server.cmd
+++ test/start-web-server.cmd
@@ -1,7 +1,9 @@
-:: Start python web server to serve current directory on specified port
+:: Starts python web server to serve parent directory on specified port
 ::     See also https://developer.mozilla.org/en-US/docs/Learn/Common_questions/set_up_a_local_testing_server
+cd ..
 py -m http.server 8000
 
+:: Don't auto-close the window when problems are encountered
 if NOT ["%errorlevel%"]==["0"] (
     pause
     exit /b %errorlevel%
