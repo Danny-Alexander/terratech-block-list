b6f8b46ec73a5278fc981f0000bfef33e8f793d0
danny-alexander <danny.alexa@gmail.com>
2019-08-31 12:51:21 +0930

    Attempt to use global variable for dataSource

--- scripts/version-selection.js
+++ scripts/version-selection.js
@@ -1,21 +1,32 @@
+
+function versionNameToDataSource(name) {
+    return 'data/TerraTech/' + name + '/blocks.json'
+}
+
 function populateVersionDropdown() {
     // Adapted from https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json
     let dropdown = $('#version-dropdown');
 
     dropdown.empty();
 
-    // dropdown.append('<option selected="true" disabled>Choose State/Province</option>');
-    // dropdown.prop('selectedIndex', 0);
-
     const dataSource = 'data/versions.json';
 
     // Populate dropdown with list of TerraTech versions
     $.getJSON(dataSource, function (data) {
+        // ToDo: Parse data and flag which version should be selected by default.
+        // Select first version by default
+        if (data.length > 0) {
+            data[0].select = true;
+            globalTableDataSource = versionNameToDataSource(data[0].name);
+            // console.log('globalTableDataSource: ' + globalTableDataSource);
+        };
         $.each(data, function (key, v) {
-            dropdown.append($('<option></option>')
-            .attr('value', v.name)
-            .attr('dataLocation', 'data/TerraTech/' + v.name)
-            .text(v.name));
+            dropdown.append($('<option' + (v.select ? ' selected' : '') + '></option>')
+                .attr('value', versionNameToDataSource(v.name))
+                .text(v.name));
         })
     });
+    
+    // console.log('globalTableDataSource: ' + globalTableDataSource);
+    return globalTableDataSource
 }
