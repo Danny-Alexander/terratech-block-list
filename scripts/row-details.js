0b47d5bcb4672ccc49f21cd09c7ea46eed1290a8
Danny Alexander <danny.alexa@gmail.com>
2019-09-05 16:03:02 +0930

    Add remaining available wiki code properties

--- scripts/row-details.js
+++ scripts/row-details.js
@@ -45,37 +45,52 @@ function wikiBlockInfoboxText(b) {
     result += "| hp              = " + b.health + "\n"; //The maximum health of the block.
     result += "| mass            = " + b.mass + "\n"; //The weight of the entire block.
     result += "| corp            = " + b.corp + "\n"; //Corporation the block is assigned to. 
-    result += "| license         = " + "#no-data#" + "\n"; //The associated license in the corp the block belongs to
+    result += "| license         = " + (b.grade + 1) + "\n"; //The associated license in the corp the block belongs to
     result += "| rarity          = " + "#no-data#" + "\n"; //The rarity of the Block
     result += "| blocklimit      = " + "#no-data#" + "\n"; //The amount of the block limit the block takes up on consoles
 
     if (b.recipe) {
-        if (b.recipe.length >= 1) {
-            result += "| ingredient1     = " + b.recipe[0].name + "\n"; //The name of the first ingredient
-            result += "| ingredient1Uses = " + b.recipe[0].count + "\n"; //The number of times its needed
+        var index, ingredient;
+        for (index = 0; index < b.recipe.length; ++index) {
+            ingredient = b.recipe[index];
+            result += "| ingredient" + (index + 1) + "     = " + ingredient.name + "\n"; //The name of the ingredient
+            result += "| ingredient" + (index + 1) + "Uses = " + ingredient.count + "\n"; //The number of times its needed
         }
-        result += "| ingredient2     = " + "#no-data#" + "\n"; //The name of the second ingredient
-        result += "| ingredient2Uses = " + "#no-data#" + "\n"; //The number of times its needed
-        result += "| ingredient3     = " + "#no-data#" + "\n"; //The name of the third ingredient
-        result += "| ingredient3Uses = " + "#no-data#" + "\n"; //The number of times its needed
-        result += "| ingredient4     = " + "#no-data#" + "\n"; //The name of the fourth ingredient
-        result += "| ingredient4Uses = " + "#no-data#" + "\n"; //The number of times its needed
-        result += "| ingredient5     = " + "#no-data#" + "\n"; //The name of the fifth ingredient
-        result += "| ingredient5Uses = " + "#no-data#" + "\n"; //The number of times its needed
-        result += "| ingredient6     = " + "#no-data#" + "\n"; //The name of the sixth ingredient
-        result += "| ingredient6Uses = " + "#no-data#" + "\n"; //The number of times its needed
     }
     result += "| value           = " + b.price + "\n"; //The purchase value
     result += "| scrapvalue      = " + "#no-data#" + "\n"; //The value of the block when it is scrapped
     result += "| traction        = " + "#no-data#" + "\n"; //Full / Partial / None
-    result += "| energycapacity  = " + "#no-data#" + "\n"; //The highest storage capacity of a battery
-    result += "| fuelcapacity    = " + "#no-data#" + "\n"; //The highest storage capacity of a fuel tank
-    result += "| fuelrefill      = " + "#no-data#" + "\n"; //The rate at which fuel tanks refill fuel
-    result += "| shotcooldown    = " + "#no-data#" + "\n"; //The time it takes for a shot to cool down
-    result += "| shotspray       = " + "#no-data#" + "\n"; //The accuracy
-    result += "| shotvelocity    = " + "#no-data#" + "\n"; //The projectile's velocity
-    result += "| shotkickback    = " + "#no-data#" + "\n"; //The kickback of the projectile
-    result += "| rotatespeed     = " + "#no-data#" + "\n"; //The speed at which a weapon rotates towards an enemy
+
+    result += (b.ModuleEnergyStore && b.ModuleEnergyStore.capacity) ? 
+        "| energycapacity  = " + b.ModuleEnergyStore.capacity + "\n" : ""; //The highest storage capacity of a battery
+
+    result += (b.ModuleFuelTank && b.ModuleFuelTank.capacity) ? 
+        "| fuelcapacity    = " + b.ModuleFuelTank.capacity + "\n" : ""; //The highest storage capacity of a fuel tank
+    
+    result += (b.ModuleFuelTank && b.ModuleFuelTank.refill_rate) ? 
+        "| fuelrefill      = " + b.ModuleFuelTank.refill_rate + "\n" : ""; //The rate at which fuel tanks refill fuel
+    
+    result += (b.ModuleWeapon && b.ModuleWeapon.shot_cooldown) ? 
+        "| shotcooldown    = " + b.ModuleWeapon.shot_cooldown + "\n" : ""; //The time it takes for a shot to cool down
+    
+    result += (b.FireData && b.FireData.bullet_spray_variance) ? 
+        "| shotspray       = " + b.FireData.bullet_spray_variance + "\n" : ""; //The accuracy
+    
+    result += (b.FireData && b.FireData.bullet_velocity) ? 
+        "| shotvelocity    = " + b.FireData.bullet_velocity + "\n" : ""; //The projectile's velocity
+
+    result += (b.FireData && b.FireData.damage) ? 
+        "| damage          = " + b.FireData.damage + "\n" : ""; //The projectile's damage
+
+    result += (b.FireData && b.FireData.damage_type) ? 
+        "| damagetype      = " + b.FireData.damage_type + "\n" : ""; //The projectile's damage type
+    
+    result += (b.FireData && b.FireData.kickback_strength) ? 
+        "| shotkickback    = " + b.FireData.kickback_strength + "\n" : ""; //The kickback of the projectile
+    
+    result += (b.ModuleWeapon && b.ModuleWeapon.rotate_speed) ? 
+        "| rotatespeed     = " + b.ModuleWeapon.rotate_speed + "\n" : ""; //The speed at which a weapon rotates towards an enemy
+    
     result += "| description     = " + b.description + "\n"; //The in-game description of a block
     result += "}}"
     return result
@@ -96,7 +111,7 @@ function rowDetailsHtml ( d ) {
         '</tr>'+
         '<tr style="background-color: #ffffff;">'+
             '<td style="vertical-align: text-top;">Wiki Block Infobox:</td>'+
-            '<td><div><a>Copy to clipboard (not implemented)</a></div><span style="white-space: pre; font-family: monospace;">' + wikiBlockInfoboxText(d) + '</span></td>'+
+            '<td><div><a>Copy to clipboard (Example only, not working)</a></div><span style="white-space: pre; font-family: monospace;">' + wikiBlockInfoboxText(d) + '</span></td>'+
         '</tr>'+
         '<tr style="background-color: #ffffff;">'+
             '<td>Resource name:</td>'+
